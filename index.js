dbef0d0ec54e94f4c401df42d16cea9e051d2487
anurudh-72 <97913332+anurudh-72@users.noreply.github.com>
2022-07-03 19:48:17 +0530

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.send('Welcome');
 })
 
-const PORT = process.env.PORT || 0.0
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}'))) 
